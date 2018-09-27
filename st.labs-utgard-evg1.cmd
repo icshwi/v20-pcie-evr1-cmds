68c4f15c6b545ce95852117a1982263f1b98aa5c
Wayne Lewis <wayne.lewis@esss.se>
2018-09-27 13:07:39 +0200

    Convert to E3 startup script

--- st.labs-utgard-evg1.cmd
+++ st.labs-utgard-evg1.cmd
@@ -1,38 +1,96 @@
-require mrfioc2,2.2.0-rc2
-#require pev,0.1.2
-
-epicsEnvSet(        "SYS", "LabS-Utgard-VIP:")
-epicsEnvSet(        "EVG",        "TS-EVG-1:")
-# epicsEnvSet("EVG_VMESLOT",            "3")
-
-#mrmEvgSetupVME($(EVG), $(EVG_VMESLOT), 0x100000, 1, 0x01)
-
-
-# ESS ICS Definition
-epicsEnvSet("HWEVT"    "14")
-epicsEnvSet("EVTFREQ"  "14")
-
-# Fake Timestamp
-# epicsEnvSet("EVRTSE"   "-2")
-
-# Don't change HBEVT
-epicsEnvSet("HBEVT"   "122")
-# One can change, but don't change it.
-epicsEnvSet("HBFREQ"    "1")
-
-# --------------------------------------------------------
-# Set Heart Beat Event (Evtcode, Fre, TrigSrc7) (122, 1, 1)
-# Set 14 Hz      Event (Evtcode, Fre, TrigSrc0) (14, 14, 1)
+# This the full setup for the Timing System with E3.
 #
-dbLoadRecords("evg-vme-230.db", "DEVICE=$(EVG), SYS=$(SYS), EvtClk-FracSynFreq-SP=88.0519, TrigEvt7-EvtCode-SP=$(HBEVT), Mxc2-Frequency-SP=$(HBFREQ), Mxc2-TrigSrc7-SP=1, TrigEvt0-EvtCode-SP=$(HWEVT), Mxc0-Frequency-SP=$(EVTFREQ), Mxc0-TrigSrc0-SP=1, SoftEvt-Enable-Sel=1")
 
-iocInit
+require mrfioc2, 2.2.0-rc2
+#require iocStats, 1856ef5
+#require autosave, 5.8.0
 
-# Fake timestamp source for testing without real hardware timestamp source (e.g., GPS recevier)
-mrmEvgSoftTime($(EVG))
+epicsEnvSet("ENGINEER","Nicklas Holmberg")
+epicsEnvSet("LOCATION","Utgard")
 
-sleep(10)
+epicsEnvSet("SYS", "LabS-Utgard-VIP:")
+epicsEnvSet("IOC", "Labs-Utgard:CT-IOC-EVG1")
+epicsEnvSet("DEV1", "TS-EVG-1:")
+epicsEnvSet("EVG", "EVG-1")
 
-# Mandatory if Fake timestamp source for testing without real hardware timestamp source (e.g., GPS recevier)
-#
-dbpf $(SYS)-$(EVG):SyncTimestamp-Cmd 1
+epicsEnvSet("MainEvtCODE" "14")
+epicsEnvSet("HeartBeatEvtCODE"   "122")
+epicsEnvSet("ESSEvtClockRate"  "88.0525")
+
+mrmEvgSetupPCI($(DEV1), "6:0d.0")
+dbLoadRecords("evg-cpci-230-ess.db",  "SYS=$(IOC), D=$(DEV1), EVG=$(EVG), FEVT=$(ESSEvtClockRate), FRF=$(ESSEvtClockRate), FDIV=1")
+
+# iocStats
+dbLoadRecords("iocAdminSoft.db", "IOC=$(IOC)-IocStats")
+
+
+# Auto save/restore
+
+# Directory should be existent before IOC runing
+#epicsEnvSet("AUTOSAVE", "/home/timinguser/autosave")
+epicsEnvSet("AUTOSAVE", "/epics/autosave")
+
+#var save_restoreDebug 1
+
+dbLoadRecords("save_restoreStatus.db", "P=$(IOC):Autosave")
+save_restoreSet_status_prefix("$(IOC):Autosave")
+set_savefile_path("${AUTOSAVE}/$(IOC)")
+set_requestfile_path("${AUTOSAVE}")
+set_pass0_restoreFile("mrf_settings.sav")
+set_pass0_restoreFile("mrf_values.sav")
+set_pass1_restoreFile("mrf_values.sav")
+set_pass1_restoreFile("mrf_waveforms.sav")
+
+
+iocInit()
+
+
+#dbl > "${IOC}_PVs.list"
+
+
+makeAutosaveFileFromDbInfo("${AUTOSAVE}/mrf_settings.req",  "autosaveFields_pass0")
+makeAutosaveFileFromDbInfo("${AUTOSAVE}/mrf_values.req",    "autosaveFields")
+makeAutosaveFileFromDbInfo("${AUTOSAVE}/mrf_waveforms.req", "autosaveFields_pass1")
+
+create_monitor_set("mrf_settings.req",   5 , "")
+create_monitor_set("mrf_values.req",     5 , "")
+create_monitor_set("mrf_waveforms.req", 30 , "")
+
+
+############### Configure RF input ##########
+#dbpf $(IOC)-$(DEV1):EvtClk-Source-Sel "RF"
+#dbpf $(IOC)-$(DEV1):EvtClk-RFFreq-SP 88.0525
+#dbpf $(IOC)-$(DEV1):EvtClk-RFDiv-SP 1
+############### Configure RF input ##########
+
+dbpf "$(IOC)-$(DEV1):1ppsInp-Sel" "Sys Clk"
+############## Configure front panel for evr 125 1 Hz ##############
+#dbpf $(IOC)-$(DEV1):TrigEvt1-EvtCode-SP 125
+#dbpf $(IOC)-$(DEV1):TrigEvt1-TrigSrc-Sel "Univ0"
+#dbpf $(IOC)-$(DEV1):1ppsInp-Sel "Univ0"
+#dbpf $(IOC)-$(DEV1):1ppsInp-MbbiDir_.TPRO 1
+############## Configure front panel for evr 125 1 Hz ##############
+
+############## Master Event Rate 14 Hz ##############
+dbpf $(IOC)-$(DEV1):Mxc0-Prescaler-SP 6289464
+#dbpf $(IOC)-$(DEV1):TrigEvt0-EvtCode-SP $(MainEvtCODE)
+#dbpf $(IOC)-$(DEV1):TrigEvt0-TrigSrc-Sel "Mxc0"
+# Setup of sequencer
+dbpf $(IOC)-$(DEV1):SoftSeq0-RunMode-Sel "Normal"
+dbpf $(IOC)-$(DEV1):SoftSeq0-TrigSrc-Sel "Mxc0"
+dbpf $(IOC)-$(DEV1):SoftSeq0-TsResolution-Sel "uSec"
+dbpf $(IOC)-$(DEV1):SoftSeq0-Load-Cmd 1
+dbpf $(IOC)-$(DEV1):SoftSeq0-Enable-Cmd 1
+# Load the sequence
+system("/bin/sh ./configure_sequencer_14Hz.sh $(IOC) $(DEV1)")
+############## Master Event Rate 14 Hz ##############
+
+# # Heart Beat 1 Hz
+dbpf $(IOC)-$(DEV1):Mxc7-Prescaler-SP 88052500
+dbpf $(IOC)-$(DEV1):TrigEvt7-EvtCode-SP $(HeartBeatEvtCODE)
+dbpf $(IOC)-$(DEV1):TrigEvt7-TrigSrc-Sel "Mxc7"
+
+epicsThreadSleep 5
+dbpf $(IOC)-$(DEV1):SyncTimestamp-Cmd 1
+
+

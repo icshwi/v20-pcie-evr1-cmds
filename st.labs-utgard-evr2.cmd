5be7b26ae0a4e5c382e22d1159a8f5d0b477e0f4
Wayne Lewis <wayne.lewis@esss.se>
2018-10-12 01:21:50 +0200

    Update EVR-2 script to use standard EVR startup script

--- st.labs-utgard-evr2.cmd
+++ st.labs-utgard-evr2.cmd
@@ -3,8 +3,10 @@ epicsEnvSet("PCI_SLOT", "1:0.0")
 epicsEnvSet("DEVICE", "EVR-2")
 epicsEnvSet("EVR", "$(DEVICE)")
 epicsEnvSet("CHIC_SYS", "Lab-Utgard-VIP:")
-epicsEnvSet("CHOP_DRV", "Chop-CHIC-")
+epicsEnvSet("CHOP_DRV", "Chop-CHIC-02")
 epicsEnvSet("CHIC_DEV", "TS-$(DEVICE)")
 
-# TODO: Work out a better way of identifying this path
-< "/epics/iocs/cmds/mrfioc2-common/st.evr.cmd"
+epicsEnvSet("E3_MODULES", "/epics/modules/e3")
+epicsEnvSet("EPICS_CMDS", "/epics/iocs/cmds")
+
+< "$(EPICS_CMDS)/mrfioc2-common/st.evr.cmd"

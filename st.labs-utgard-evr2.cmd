3186c656695ea8af1424b0ade231eed53ce025f1
Wayne Lewis <wayne.lewis@esss.se>
2018-10-12 00:45:34 +0200

    Update macros

--- st.labs-utgard-evr2.cmd
+++ st.labs-utgard-evr2.cmd
@@ -2,7 +2,9 @@ epicsEnvSet("SYS", "LabS-Utgard-VIP:TS")
 epicsEnvSet("PCI_SLOT", "1:0.0")
 epicsEnvSet("DEVICE", "EVR-2")
 epicsEnvSet("EVR", "$(DEVICE)")
-epicsEnvSet("CHICSYS", "Lab-Utgard-VIP")
-epicsEnvSet("CHICID", "Chop-CHIC-02")
+epicsEnvSet("CHIC_SYS", "Lab-Utgard-VIP:")
+epicsEnvSet("CHOP_DRV", "Chop-CHIC-")
+epicsEnvSet("CHIC_DEV", "TS-$(DEVICE)")
+
 # TODO: Work out a better way of identifying this path
 < "/epics/iocs/cmds/mrfioc2-common/st.evr.cmd"

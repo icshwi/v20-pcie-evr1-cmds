f74633de172aa067920379f8be91b632458fa138
Wayne Lewis <wayne.lewis@esss.se>
2018-10-16 19:14:37 +0200

    Update PV names

--- st.labs-utgard-evr2.cmd
+++ st.labs-utgard-evr2.cmd
@@ -2,11 +2,11 @@ epicsEnvSet("SYS", "LabS-Utgard-VIP:TS")
 epicsEnvSet("PCI_SLOT", "1:0.0")
 epicsEnvSet("DEVICE", "EVR-2")
 epicsEnvSet("EVR", "$(DEVICE)")
-epicsEnvSet("CHIC_SYS", "Lab-Utgard-VIP:")
-epicsEnvSet("CHOP_DRV", "Chop-CHIC-02")
+epicsEnvSet("CHIC_SYS", "LabS-Utgard-VIP:")
+epicsEnvSet("CHOP_DRV", "Chop-Drv-02")
 epicsEnvSet("CHIC_DEV", "TS-$(DEVICE)")
 
-epicsEnvSet("E3_MODULES", "/epics/modules/e3")
+epicsEnvSet("E3_MODULES", "/epics/iocs/e3")
 epicsEnvSet("EPICS_CMDS", "/epics/iocs/cmds")
 
 < "$(EPICS_CMDS)/mrfioc2-common/st.evr.cmd"

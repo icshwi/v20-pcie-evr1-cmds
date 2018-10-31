e5d645d15dab556db8f8e9e7c3c68b5ff1b85b4e
root <nicklas.holmberg@esss.se>
2018-10-31 14:21:04 +0100

    Require updated from 3.0.0 to 3.0.2

--- start_background.sh
+++ start_background.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export IOC_EXEC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 export VAR_DIR="/var"
-export E3_BIN_DIR="/epics/base-3.15.5/require/3.0.0/bin"
+export E3_BIN_DIR="/epics/base-3.15.5/require/3.0.2/bin"
 export PROCSERV="/usr/bin/procServ"
 export PROCSERV_PORT=2000
 export PROCSERV_IOC_DIR=labs-utgard-evr2
